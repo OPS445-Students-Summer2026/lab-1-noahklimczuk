7803c58ad9789cfc7a3f5291540540810fd52cf7
Noah Klimczuk <nklimczuk@myseneca.ca>
2026-05-22 14:02:09 -0400

    added lab1a

---
+++ lab1a.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Description: this program will output hello world to the screen '''
+
+# any lines that start with a # is also known as a comment
+# these lines are ignored by the interpreter even if 
+# the contain code. the very first like is called a shebang line 
+# it is used to tell the system which interpreter to
+# use python2 python3 bash ect
+
+
+print('Hello world')
